5a8d6af6800177188070ead1cc943d107fa998a0
Gary Jacobs <gjacobs@trafficcast.com>
2020-08-27 21:32:42 -0400

    Fix build, update library versions

--- app/src/main/java/com/gmjproductions/gjtwittersearch/ui/widgets/ComboBox.kt
+++ app/src/main/java/com/gmjproductions/gjtwittersearch/ui/widgets/ComboBox.kt
@@ -17,9 +17,9 @@ class ComboBox : AppCompatAutoCompleteTextView {
 
     private var listener: View.OnClickListener? = null
 
-    constructor(context: Context?) : super(context)
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
 
     init {
